e1b2e6be0164a7cbc834fe7bdcf7519039c0c4af
Dibya452 <happydibya989@gmail.com>
2026-05-04 00:01:01 +0530

    Change server port to use environment variable

--- server/server.js
+++ server/server.js
@@ -63,5 +63,5 @@ app.use("/api/message", messagerouter);
 //Connect to database
 await connectDB();
 
-const PORT ="https://chat-app-backend-lfw4.onrender.com";
+const PORT =process.env.PORT || 5002;
 server.listen(PORT, ()=>console.log("server is running on port:"+ PORT));
